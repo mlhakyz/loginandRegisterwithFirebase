03ddca676e2c82f16d0bc9aa6bcc27a0ff8bfe86
Melih Akyuz <melihakyuz@Melihs-MacBook-Pro.local>
2023-11-16 17:31:22 +0300

    uptade profile username and profile picture

--- app/src/main/java/com/example/loginregisterwithfirebase/MainActivity.kt
+++ app/src/main/java/com/example/loginregisterwithfirebase/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.loginregisterwithfirebase
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.userProfileChangeRequest
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -22,8 +26,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var passwordReg: EditText
     lateinit var emailLog: EditText
     lateinit var passwordLog: EditText
+    lateinit var usernamebir: EditText
     lateinit var tvLoggedIn: TextView
     lateinit var btnRegister: Button
+    lateinit var btnUpdate: Button
+    lateinit var ivProfilePicture: ImageView
+
     lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,16 +40,49 @@ class MainActivity : AppCompatActivity() {
         passwordReg = findViewById(R.id.etPasswordRegister)
         emailLog = findViewById(R.id.etEmailLogin)
         passwordLog = findViewById(R.id.etPasswordLogin)
+        usernamebir = findViewById(R.id.etUsername)
         tvLoggedIn = findViewById(R.id.tvLoggedIn)
         btnRegister = findViewById(R.id.btnRegister)
         btnLogin = findViewById(R.id.btnLogin)
+        btnUpdate = findViewById(R.id.btnUpdateProfile)
+        ivProfilePicture = findViewById(R.id.ivProfilePicture)
+
         auth = FirebaseAuth.getInstance()
-auth.signOut()
         btnRegister.setOnClickListener {
             registerUser()
         }
         btnLogin.setOnClickListener {
             loginUser()
+        }
+        btnUpdate.setOnClickListener {
+            updateProfile()
+        }
+    }
+
+    private fun updateProfile(){
+        auth.currentUser?.let {user ->
+            val username = usernamebir.text.toString()
+            val photoURI  = Uri.parse("android.resource://$packageName/${R.drawable.logo_black_square}")
+            val profileUpdates =  UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .setPhotoUri(photoURI)
+                .build()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    user.updateProfile(profileUpdates).await()
+                    checkLoggedInState()
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity,"Successfuly updated user profile",Toast.LENGTH_LONG).show()
+
+                    }
+                }catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+
+                    }
+                }
+            }
         }
     }
 
@@ -73,10 +114,14 @@ auth.signOut()
     }
 
     private fun checkLoggedInState(){
-        if (auth.currentUser == null){
+        val user = auth.currentUser
+        if (user == null){
             tvLoggedIn.text = "You are not Logged in"
         }else{
             tvLoggedIn.text = "You are Logged in"
+            usernamebir.setText(user.displayName)
+            ivProfilePicture.setImageURI(user.photoUrl)
+
         }
 
     }
